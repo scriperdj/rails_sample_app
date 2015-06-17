af722d8411478cd622760b195d0dd8e639b33702
scriperdj <scriperdj@gmail.com>
2015-06-17 23:50:01 +0530

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+  
   def new
+    @user = User.new
     @title = "Sign up"
   end
   
@@ -7,4 +9,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @title = @user.name
   end
+  
+  def create
+    @user = User.new(params[:user].permit(:name, :email, :password, :password_confirmation))
+    if @user.save
+      #save user info
+      flash[:success] = "Welcome to Rails sample app"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+  
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -47,5 +47,46 @@ RSpec.describe UsersController, type: :controller do
 	end
  
   end
+  
+  describe "POST 'create'" do
+    describe "failure" do
+      before(:each) do
+        @attr = {:name=>"", :email=>"", :password=>"",:password_confirmation=>""}
+      end
+      it "should not create new user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+      it "should have the right title" do
+	    post :create, :user => @attr
+	    expect(response.body).to have_title("Sign up")
+	  end
+	  it "should render the 'new' page" do
+    	post :create, :user => @attr
+	    response.should render_template('new')
+	  end
+	end
+	
+	describe "success" do
+	  before(:each) do
+	    @attr = {:name=>"Mohan", :email=>"mohan@gmail.com", :password=>"foobar", :password_confirmation=>"foobar"}
+	  end
+	  it "should create new user" do
+	    lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+	  end
+	  it "should redirect to user profile" do
+	    post :create, :user => @attr
+	    response.should redirect_to(user_path(assigns(:user)))
+	  end
+	  it "should have welcome message" do
+	    post :create, :user => @attr
+	    flash[:success].should =~ /welcome to rails sample app/i
+	  end
+	end
+    
+  end
 
 end 
